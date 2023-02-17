4eb4b9a903ad6573d1253114dc8bd9be5e7ef7e4
António Faneca <antonio.faneca@atlantico.eu>
2023-02-17 12:11:04 +0000

    replaced text with custom icons for filter tabs in FilterBottomSheet

--- app/src/main/java/com/afaneca/afascore/ui/matchList/components/FilterBottomSheet.kt
+++ app/src/main/java/com/afaneca/afascore/ui/matchList/components/FilterBottomSheet.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.SheetState
@@ -26,6 +28,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextOverflow
@@ -78,9 +81,9 @@ private fun FilterBottomSheetView(
     var selectedTabIndex by remember { mutableStateOf(0) }
     val coroutineScope = rememberCoroutineScope()
     val tabTitles = listOf(
-        stringResource(id = R.string.filter_by_team),
-        stringResource(id = R.string.filter_by_competitions),
-        stringResource(id = R.string.filter_by_status),
+        Pair(R.drawable.team_24dp, R.string.filter_by_team),
+        Pair(R.drawable.trophy_24dp, R.string.filter_by_competitions),
+        Pair(R.drawable.whistle_24dp, R.string.filter_by_status),
     )
     val (teams, setTeams) = remember {
         mutableStateOf(filterDataUiModel.teamsList)
@@ -107,11 +110,16 @@ private fun FilterBottomSheetView(
             Text(text = stringResource(id = R.string.filter))
         }
         TabRow(selectedTabIndex = selectedTabIndex) {
-            tabTitles.forEachIndexed { index, title ->
+            tabTitles.forEachIndexed { index, tabData ->
                 Tab(
                     selected = selectedTabIndex == index,
                     onClick = { selectedTabIndex = index },
-                    text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) })
+                    text = {
+                        Icon(
+                            painter = painterResource(id = tabData.first),
+                            contentDescription = stringResource(id = tabData.second),
+                        )
+                    })
             }
         }
 
