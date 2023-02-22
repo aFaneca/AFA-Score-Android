677bbf9845ea3ed904845dc6678ad4c4eefa2304
António Faneca <antonio.faneca@atlantico.eu>
2023-02-22 09:54:40 +0000

    ask for POST_NOTIFICATIONS permission for API >= 33 on MainActivity launch

--- app/src/main/java/com/afaneca/afascore/common/PushNotificationService.kt
+++ app/src/main/java/com/afaneca/afascore/common/PushNotificationService.kt
@@ -1,9 +1,12 @@
 package com.afaneca.afascore.common
 
+import android.annotation.TargetApi
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.afaneca.afascore.R
 import com.afaneca.afascore.ui.MainActivity
@@ -31,9 +34,14 @@ class PushNotificationService : FirebaseMessagingService() {
     private fun showNotification(message: RemoteMessage) {
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+        )
 
-        val notification = NotificationCompat.Builder(this, "CHANNELID")
+        val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
             .setContentTitle(message.data["title"])
             .setContentText(message.data["message"])
             .setSmallIcon(R.mipmap.ic_launcher)
@@ -41,8 +49,26 @@ class PushNotificationService : FirebaseMessagingService() {
             .setContentIntent(pendingIntent)
             .build()
 
-        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        manager.notify(Random.nextInt(), notification)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(createNotificationChannel())
+        }
+
+        notificationManager.notify(Random.nextInt(), notification)
     }
 
+    @TargetApi(26)
+    private fun createNotificationChannel(): NotificationChannel {
+        val ctx: Context = this
+        val name: CharSequence = ctx.getString(R.string.notification_channel_name)
+        val description = ctx.getString(R.string.notification_channel_description)
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance)
+        channel.description = description
+        return channel
+    }
+
+    companion object {
+        const val NOTIFICATION_CHANNEL_ID = "afascore-notif-id"
+    }
 }
--- app/src/main/java/com/afaneca/afascore/ui/MainActivity.kt
+++ app/src/main/java/com/afaneca/afascore/ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.afaneca.afascore.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -30,6 +35,17 @@ import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Notifications are now enabled
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,7 +70,27 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener { Timber.d("PushNotification KEY: " + it.result) }
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { Timber.d("PushNotification FCM KEY: " + it.result) }
+        askNotificationPermission()
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // Notifications are enabled
+            } /*else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            }*/ else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
     }
 
     @Composable
